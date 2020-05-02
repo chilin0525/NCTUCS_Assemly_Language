e140ad224ec302c4ccf53019e7da6a351e6816e3
7070707070 <sky2502211@gmail.com>
2020-05-03 07:44:12 +0800

    8th commit

---
+++ Reverse_data_2.asm
@@ -0,0 +1,29 @@
+ORG 0
+mov 30h,#1
+mov 31h,#2
+mov 32h,#3
+mov 33h,#4
+mov 34h,#5
+mov 35h,#6
+mov 36h,#7
+mov 37h,#8
+
+
+push 30H
+push 31H
+push 32H
+push 33H
+push 34H
+push 35H
+push 36H
+push 37H
+pop 40H
+pop 41H
+pop 42H
+pop 43H
+pop 44H
+pop 45H
+pop 46H
+pop 47H
+
+END
